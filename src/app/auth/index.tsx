7432d16e4143c1ce413d31e8071e4c84cf7ad330
Aimable01 <aimable.kwizera14@gmail.com>
2026-05-19 16:21:47 +0200

    feat: login page

--- src/app/auth/index.tsx
+++ src/app/auth/index.tsx
@@ -1,15 +1,47 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import api from "../../../services/axios";
 
 export default function LoginPage() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = async () => {
+    if (!username || !password) {
+      Alert.alert("Validation", "All fields are required");
+      return;
+    }
+    try {
+      const response = await api.get(`/users?username=${username}`);
+
+      const user = response.data[0];
+
+      if (user && user.password === password) {
+        navigation.navigate("/");
+      } else {
+        Alert.alert("Error", "Invalid credentials");
+      }
+    } catch (error) {
+      Alert.alert("Error", "Something went wrong!");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text style={styles.text}>Login</Text>
       <View>
-        <TextInput placeholder="Username" style={styles.input} />
+        <TextInput
+          placeholder="Username"
+          style={styles.input}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
         <TextInput
           placeholder="Password"
           secureTextEntry
           style={styles.input}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
         />
         <Button title="Login" />
       </View>
@@ -29,5 +61,10 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 15,
     borderRadius: 5,
+  },
+  text: {
+    marginBottom: 15,
+    fontSize: 18,
+    textAlign: "center",
   },
 });
